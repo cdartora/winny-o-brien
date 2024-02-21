00bafc5d8880d988d3dfe5522ce1dd92975d335b
Carlos Dartora <carlosdalsoler@gmail.com>
2024-02-21 00:44:24 -0300

    feat: add threads to sidebar

--- src/app/side-bar.js
+++ src/app/side-bar.js
@@ -5,6 +5,15 @@ const SideBar = ({ children }) => {
     <>
       <div className="fixed z-10 bottom-0 left-0 pl-3 pb-3 md:pl-8 md:pb-8 flex flex-col justify-end items-center gap-6">
         <a
+          href="https://www.instagram.com/winnyobrien/"
+          target="_blank"
+          rel="noreferrer"
+          className="shadow-2xl opacity-60 hover:opacity-100 transition-all"
+        >
+          <Image src="/threads.svg" alt="Threads logo" width="42" height="42" />
+        </a>
+
+        <a
           href="https://wa.me/555189430417"
           target="_blank"
           rel="noreferrer"
