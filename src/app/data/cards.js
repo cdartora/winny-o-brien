1684f4fe27ddc3bbbe6ccd753569371bdd4e242e
Carlos Dartora <carlosdalsoler@gmail.com>
2024-04-25 16:32:43 -0300

    data: organize data in new folder

---
+++ src/app/data/cards.js
@@ -0,0 +1,24 @@
+export const vip_cards = [
+  {
+    name: "volte a si",
+    text1:
+      "A Cruz Celta é composta por dez cartas dispostas de maneira especíﬁca. No centro, a primeira carta representa o tema central da leitura.",
+    text2:
+      "A Cruz Celta é composta por dez cartas dispostas de maneira especíﬁca. No centro, a primeira carta representa o tema central da leitura.",
+    duration: "4 meses",
+    imgUrl: "/tarot/1.png",
+    price: 650,
+    type: "Tarot, Reiki",
+  },
+  {
+    name: "atendimento vip",
+    text1:
+      "A Cruz Celta é composta por dez cartas dispostas de maneira especíﬁca. No centro, a primeira carta representa o tema central da leitura.",
+    text2:
+      "A Cruz Celta é composta por dez cartas dispostas de maneira especíﬁca. No centro, a primeira carta representa o tema central da leitura.",
+    duration: "4 meses",
+    imgUrl: "/tarot/1.png",
+    price: 650,
+    type: "Tarot, Reiki",
+  },
+];
