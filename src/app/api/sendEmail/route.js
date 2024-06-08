97ccb255c2b74f91a12d4b92abe48cd29a33fac1
Carlos Dartora <carlosdalsoler@gmail.com>
2024-06-08 17:25:37 -0300

    feat: carousel cards data fetch from cms

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -1,31 +1,31 @@
-import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(request) {
   const { name, email, message } = await request.json();
 
   const transporter = nodemailer.createTransport({
     service: "Gmail",
-    host: 'smtp.gmail.com', // Substitua pelo seu host SMTP
-    port: 587, // Substitua pela porta correta
-    secure: false, // true para port 465, false para outras portas
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: 'carlosdalsoler@gmail.com', // Seu email
-      pass: 'rrdw ivdu bckq ocww', // Sua senha
+      user: "carlosdalsoler@gmail.com",
+      pass: process.env.SMTP_PASS,
     },
   });
 
   try {
     await transporter.sendMail({
       from: email,
-      to: 'carlosdalsoler@gmail.com', // Substitua pelo email do destinatário
+      to: "carlosdalsoler@gmail.com",
       subject: `New message from ${name}`,
       text: message,
       html: `<p><strong>Name:</strong> ${name}</p><p><strong>Email:</strong> ${email}</p><p><strong>Message:</strong> ${message}</p>`,
     });
 
-    return NextResponse.json({ message: 'Email sent successfully' });
+    return NextResponse.json({ message: "Email sent successfully" });
   } catch (error) {
-    return NextResponse.json({ error: 'Error sending email' }, { status: 500 });
+    return NextResponse.json({ error: "Error sending email" }, { status: 500 });
   }
 }
--- src/app/components/carousel.js
+++ src/app/components/carousel.js
@@ -1,13 +1,60 @@
-"use client"
+"use client";
 import { useState, useEffect, useRef } from "react";
 import Card from "./card";
 import { cakelan } from "../page";
-import { cards } from "../data/cards";
-import './carousel.css';
+import "./carousel.css";
+function handleData(data) {
+  console.log(data);
+  return data.map((card, index) => ({
+    number: card.number,
+    name: card.name,
+    summary: card.summary,
+    descriptions: [card.description1, card.description2],
+    durations: [
+      {
+        time: 30,
+        title: "Leitura Completa (30min)",
+        description: "Uma leitura detalhada e abrangente.",
+        price: card["30"],
+        image: "duration",
+      },
+      {
+        time: 45,
+        title: "Leitura Equilibrada (45min)",
+        description:
+          "Uma leitura balanceada, abrangendo várias questões com uma profundidade moderada.",
+        price: card["45"],
+        image: "duration",
+      },
+      {
+        time: 60,
+        title: "Leitura Profunda (60min)",
+        description:
+          "Uma leitura extensa e minuciosa, ideal para explorar temas complexos e obter insights aprofundados.",
+        price: card["60"],
+        image: "duration-more",
+      },
+    ],
+    imgUrl: `tarot/${index + 1}.svg`,
+  }));
+}
 
 function Carousel() {
   const carouselRef = useRef(null);
   const [centeredIndex, setCenteredIndex] = useState(0);
+  const [cards, setCards] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console.log(process.env.CARDS_API_URL);
+    fetch(process.env.CARDS_API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        const cardsData = handleData(data);
+        setCards(cardsData);
+        setLoading(false);
+      });
+  }, []);
 
   useEffect(() => {
     const carousel = carouselRef.current;
@@ -35,12 +82,12 @@ function Carousel() {
     };
 
     handleScroll(); // Initial call to set the first centered element
-    carousel.addEventListener('scroll', handleScroll);
+    carousel.addEventListener("scroll", handleScroll);
 
     return () => {
-      carousel.removeEventListener('scroll', handleScroll);
+      carousel.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [cards]);
 
   useEffect(() => {
     const cards = carouselRef.current.querySelectorAll(".card");
@@ -55,38 +102,33 @@ function Carousel() {
 
   return (
     <div id="tarot" className="carousel-container">
-        <h2
-          className={`${cakelan.className}`}
-        >
-          ferramentas
-        </h2>
+      <h2 className={`${cakelan.className}`}>ferramentas</h2>
 
-        <p>
-          As cartas do tarot e do baralho cigano atuam como instrumentos de
-          conexão com o campo energético, permitindo-me oferecer uma perspectiva
-          detalhada e precisa sobre diversas situações, auxiliando-o na tomada
-          de decisões que facilitarão a resolução de seus problemas.
-        </p>
+      <p>
+        As cartas do tarot e do baralho cigano atuam como instrumentos de
+        conexão com o campo energético, permitindo-me oferecer uma perspectiva
+        detalhada e precisa sobre diversas situações, auxiliando-o na tomada de
+        decisões que facilitarão a resolução de seus problemas.
+      </p>
 
-        <h2 className={`${cakelan.className}`}>
-          leituras
-        </h2>
+      <h2 className={`${cakelan.className}`}>leituras</h2>
 
-        <p>
-          A arte da cartomancia é uma prática oracular ancestral que nos permite
-          desvendar os mistérios do nosso inconsciente, proporcionando uma
-          compreensão mais profunda das questões que enfrentamos no dia a dia.
-          <br />
-          Minhas leituras de tarot e baralho cigano são guiadas por uma
-          abordagem de mentoria espiritual, com o objetivo de orientá-lo através
-          de seus sentimentos, desejos e possibilidades.
-        </p>
-
+      <p>
+        A arte da cartomancia é uma prática oracular ancestral que nos permite
+        desvendar os mistérios do nosso inconsciente, proporcionando uma
+        compreensão mais profunda das questões que enfrentamos no dia a dia.
+        <br />
+        Minhas leituras de tarot e baralho cigano são guiadas por uma abordagem
+        de mentoria espiritual, com o objetivo de orientá-lo através de seus
+        sentimentos, desejos e possibilidades.
+      </p>
 
       <div ref={carouselRef} className="carousel">
-        {cards.map((currentCard) => (
-          <Card cardInfo={currentCard} key={currentCard}/>
-        ))}
+        {isLoading && <p>is Loading...</p>}
+        {cards &&
+          cards.map((currentCard) => (
+            <Card cardInfo={currentCard} key={currentCard} />
+          ))}
       </div>
     </div>
   );
